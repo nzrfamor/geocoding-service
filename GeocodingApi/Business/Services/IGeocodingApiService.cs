9db712d037ac60df9df9885fe4753958e275525c
Nazar Brych <nazar.brych.06@gmail.com>
2023-02-19 20:31:05 +0200

    Added IGeocodingApiService interface that contain necessary methods

---
+++ GeocodingApi/Business/Services/IGeocodingApiService.cs
@@ -0,0 +1,16 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public interface IGeocodingApiService
+    {
+        Task<GeocodingModel> GetGeocodingDataByLatLngAsync(float lat, float lng);
+        Task<GeocodingModel> GetGeocodingDataByAddressAsync(string address);
+        Task<GeocodingModel> GetGeocodingDataByPlaceIdAsync(string placeId);
+
+    }
+}
