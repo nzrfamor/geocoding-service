c4143193a61a60d2d35ac3250cd19cc769e0a8d3
Nazar Brych <nazar.brych.06@gmail.com>
2023-02-19 20:29:25 +0200

    Added model that represents necessary geocoding data

---
+++ GeocodingApi/Business/Models/GeocodingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Models
+{
+    public class GeocodingModel
+    {
+        public string FullAddress { get; set; }
+        public string PlaceId { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
